4eda745a3c480e5d7aafa7a7c74fdb5a9fe25932
GRAVITAD <synergiart.websupp@gmail.com>
2025-10-02 19:09:46 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://strapi-back.gravitad.xyz'),
-  proxy: true,
   app: { keys: env.array('APP_KEYS') },
+  url: env('URL', 'https://pizquito-strapi.gravitad.xyz'),
+  proxy: true,
+  transfer: {
+    remote: { enabled: true },                 // habilita transfer remotas
+    token: { salt: env('TRANSFER_TOKEN_SALT') } // usa tu SALT
+  },
 });
