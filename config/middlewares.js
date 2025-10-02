dae08f8ce3066bf1e06006abc60306c60618ad00
gravitad-dev <synergiart.websupp@gmail.com>
2025-10-02 23:04:47 +0000

    fix: cors url front

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,32 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
+const allowedOrigins = (process.env.CORS_ORIGINS || 'https://pizquito-front-dev.gravitad.xyz').split(',');
+
+export default [
   'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: allowedOrigins, // dominios permitidos dinámicos
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeadersOnError: true,
+    },
+  },
+  'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
